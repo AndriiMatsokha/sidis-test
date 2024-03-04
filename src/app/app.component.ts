470379ac09d2be1b02e98d63a8b3aecaf8cdb42c
andrii <andrey.matsoha@gmail.com>
2024-03-04 17:10:15 +0200

    add header, burger and block expansion [task-1][task-2][task-3]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { reverseWord } from './functions/reverse-word';
 import { generatePassword } from './functions/generate-password';
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
+  public burgerIsOpen: boolean = false;
   public ngOnInit(): void {
     // task-4
     console.log(reverseWord("молоток"));
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  @Output()
+  public onOpen = new EventEmitter<boolean>();
+  public isOpen = false;
+}
